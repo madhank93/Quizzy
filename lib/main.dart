bcac3803c36b342daa8ab12197c33413b3838ae2
madhank93 <maddymadhan946@gmail.com>
2020-01-13 22:42:28 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Quizzy'),);
+  }
+}
