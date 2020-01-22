d4dc1a6755febd803ba6706dd73d1388b7f2d02d
madhank93 <maddymadhan946@gmail.com>
2020-01-22 22:23:27 +0530

    styling the widgets

--- lib/question.dart
+++ lib/question.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String question;
-  Question(question) {
-    this.question=question;
-  }
+  final String question;
+  Question(this.question);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Text(question);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        question,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
