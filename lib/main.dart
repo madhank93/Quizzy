6f1f5a4b555af9b94b4070ae0dcde3af53bc2d15
madhank93 <maddymadhan946@gmail.com>
2020-01-26 16:52:12 +0530

    Answer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/answer.dart';
 import 'package:flutter_quiz/question.dart';
 
 void main() => runApp(QuizApp());
@@ -21,11 +22,23 @@ class _QuizAppState extends State<QuizApp> {
     });
   }
 
-  List<String> ques = ["1. In India, who heads the Department of Space?",
-  "2. Who wrote Das Kapital?",
-  "3. Which pop album is the best selling of all time?",
-  "4. Who directed the epic historical drama Schindler\'s List in 1993?",
-  "5. Which boxer inflicted Muhammad Ali\'s first defeat in professional boxing?"];
+  static const ques = [
+    {"questionText": "1. In India, who heads the Department of Space?",
+    "answers": ['The president', 'The prime minister', 'The defence minster'],
+    "correct": 'The prime minister'},
+    {"questionText":"2. Who wrote Das Kapital?",
+    "answers": ['Karl Marx', 'Friedrich Engels', 'Leon Trotsky'],
+    "correct": 'Karl Marx'},
+    {"questionText": "3. Which pop album is the best selling of all time",
+    "answers": ['Thriller by Michael Jackson', 'The Beatles by The Beatles', 'Purple Rain by Prince and The Revolution'],
+    "correct": 'Thriller by Michael Jackson'},
+    {"questionText": "4. Who directed the epic historical drama Schindler\'s List in 1993?",
+    "answers": ['James Cameron', 'Martin Scorsese', 'Steven Spielberg'],
+    "correct": 'Steven Spielberg'},
+    {"questionText": "5. Which boxer inflicted Muhammad Ali\'s first defeat in professional boxing?",
+    "answers": ['Rocky Marciano', 'Joe Frazier', 'Joe Louis'],
+    "correct": 'Joe Frazier'}
+    ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,19 +49,10 @@ class _QuizAppState extends State<QuizApp> {
         ),
         body: Column(
           children: [
-            Question(ques[_questionIndex]),
-            RaisedButton(
-              child: Text("Answer 1"),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text("Answer 2"),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text("Answer 3"),
-              onPressed: _answerQuestion,
-            )
+            Question(ques[_questionIndex]['questionText']),
+            ...(ques[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
